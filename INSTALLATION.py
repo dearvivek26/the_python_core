c5a926ecd55b9c7915861601d0026878c790136a
VIVEK KUMAR <dearvivkumar26@gmail.com>
2025-09-10 19:47:59 +0530

    Add files via upload

---
+++ INSTALLATION.py
@@ -0,0 +1,17 @@
+"""
+STEP-1
+  Download the setup of python from google._
+   install it.
+  
+STEP-2 
+    Download the VS Code according to windows
+    and then install it
+    
+    
+    
+STEP-3
+    Click on extension.
+    write python on search.
+    then install the extension of python
+    
+     """
